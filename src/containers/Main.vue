85f9e3b5a6ddab4b9892193d2263ad8145cdeea4
Charles Obeng <charlesobeng000@gmail.com>
2020-02-08 16:18:52 +0000

    project set up

---
+++ src/containers/Main.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Navbar />
+    <v-content>
+      
+    </v-content>
+  </div>
+</template>
+
+<script>
+import Navbar from "./NavBar"
+
+export default {
+  name: "Main",
+  components: {
+    Navbar
+  }
+}
+</script>
---
+++ src/containers/NavBar.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app-bar app>
+      <div class="d-flex align-center">
+        <v-img
+          alt="Logo"
+          class="shrink mr-2"
+          contain
+          src="../assets/images/logo.png"
+          transition="scale-transition"
+          width="40"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "NavBar"
+}
+</script>
