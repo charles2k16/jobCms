e80ba9538b38ae4b82eb13fc19b7dac8766644a5
Charles Obeng <charlesobeng000@gmail.com>
2020-09-09 09:38:50 +0000

    ui changes

--- src/containers/Main.vue
+++ src/containers/Main.vue
@@ -1,37 +1,79 @@
 <template>
-  <div id="main">
-    <Navbar />
-    <v-content id="content">
-      <v-container fill-height>
-        <v-row>
-          <v-col md="3">
-            <SideBar />
-          </v-col>
-          <v-col md="9">
-            <div>
-              <router-view></router-view>
-            </div>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      clipped
+      :mini-variant="miniVariant"
+    >
+      <v-list dense nav class="py-0">
+        <v-list-item two-line :class="miniVariant && 'px-0'">
+          <v-list-item-avatar>
+            <img src="https://randomuser.me/api/portraits/men/81.jpg" />
+          </v-list-item-avatar>
+
+          <v-list-item-content>
+            <v-list-item-title>Application</v-list-item-title>
+            <v-list-item-subtitle>Subtext</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-divider></v-divider>
+
+        <v-list-item v-for="item in items" :key="item.title" link>
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app color="indigo" dark clipped-left>
+      <v-toolbar-title>Application</v-toolbar-title>
+      <v-app-bar-nav-icon
+        @click.stop="miniVariant = !miniVariant"
+      ></v-app-bar-nav-icon>
+    </v-app-bar>
+
+    <v-main>
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
+          <v-col class="text-center">
+            <v-tooltip left>
+              <template v-slot:activator="{ on }">
+                <v-btn :href="source" icon large target="_blank" v-on="on">
+                  <v-icon large>mdi-code-tags</v-icon>
+                </v-btn>
+              </template>
+              <span>Source</span>
+            </v-tooltip>
           </v-col>
         </v-row>
       </v-container>
-    </v-content>
-  </div>
+    </v-main>
+    <v-footer color="indigo" app>
+      <span class="white--text">&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
-import Navbar from "./NavBar"
-import SideBar from "./SideBar"
-
 export default {
-  name: "Main",
-  components: {
-    Navbar, SideBar
-  }
-}
+  props: {
+    source: String,
+  },
+  data: () => ({
+    drawer: null,
+    miniVariant: false,
+    items: [
+      { title: 'Dashboard', icon: 'mdi-view-dashboard' },
+      { title: 'Photos', icon: 'mdi-image' },
+      { title: 'About', icon: 'mdi-help-box' },
+    ],
+  }),
+};
 </script>
-
-<style scoped>
-  #main {
-    background-color: #F6F7FB;
-  }
-</style>
--- src/containers/SideBar.vue
+++ src/containers/SideBar.vue
@@ -2,8 +2,7 @@
   <div class="sidebar">
     <div class="profile text-center">
       <v-avatar size="80">
-        <img src="../assets/images/profile.png" alt="profile"
-        >
+        <img src="../assets/images/profile.png" alt="profile" />
       </v-avatar>
       <div class="mt-4">
         <h4 class="font-weight-black"></h4>
@@ -30,29 +29,28 @@
   </div>
 </template>
 
-
 <script>
 export default {
-  data () {
+  data() {
     return {
       menu: 0,
       menuItems: [
-        { text: "Profiles", icon: "mdi-view-dashboard" },
-        { text: "Subscribers", icon: "mdi-card-bulleted-outline" },
-        { text: "Users", icon: "mdi-file-account" },
+        { text: 'Profiles', icon: 'mdi-view-dashboard' },
+        { text: 'Subscribers', icon: 'mdi-card-bulleted-outline' },
+        { text: 'Users', icon: 'mdi-file-account' },
 
-        { text: "Logout", icon: "mdi-logout" },
+        { text: 'Logout', icon: 'mdi-logout' },
       ],
-    }
-  }
-}
+    };
+  },
+};
 </script>
 
 <style>
-  .sidebar {
-    background-color: #FFFFFF;
-    border: 1px solid rgb(238, 237, 237);
-    border-radius: 5px;
-    padding-top: 15px;
-  }
+.sidebar {
+  background-color: #ffffff;
+  border: 1px solid rgb(238, 237, 237);
+  border-radius: 5px;
+  padding-top: 15px;
+}
 </style>
