75f9968d1a07d4a5261a474619dda0ae57e5848a
samuelbartels <samuel.bartels@paicbd.com>
2020-09-11 18:40:46 +0000

    adding article

--- src/views/Subscribers/List.vue
+++ src/views/Subscribers/List.vue
@@ -1,15 +1,43 @@
 <template>
-    <div>
+  <div style="width:100%">
+    <v-toolbar dense flat>
+      <v-toolbar-title>Profile / List</v-toolbar-title>
 
-    </div>
+      <v-spacer></v-spacer>
+
+      <v-text-field
+        flat
+        outlined
+        clearable
+        hide-details
+        color="#8B222F"
+        label="Search"
+        prepend-inner-icon="mdi-magnify"
+      id="search_bar"></v-text-field>
+    </v-toolbar>
+
+    <v-card class="mt-10">
+      <h5>Put table here</h5>
+    </v-card>
+  </div>
 </template>
 
 <script>
 export default {
-    
-}
+  name: 'Dashboard',
+  data() {
+    return {};
+  },
+};
 </script>
 
 <style>
-
+#search_bar {
+  top: 10px;
+  left: 852px;
+  width: 319px;
+  height: 35px;
+  background: #FFFFFF 0% 0% no-repeat padding-box;
+  opacity: 1;
+}
 </style>
