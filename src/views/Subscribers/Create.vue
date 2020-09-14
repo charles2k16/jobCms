5aa8db2c03aaa63cab5361eb9991531c3f203449
samuelbartels <samuel.bartels@paicbd.com>
2020-09-14 23:25:19 +0000

    added create subscription form

--- src/views/Subscribers/Create.vue
+++ src/views/Subscribers/Create.vue
@@ -1,43 +1,63 @@
 <template>
-  <div style="width:100%">
-    <v-toolbar dense flat>
-      <v-toolbar-title>Subscribers / Create</v-toolbar-title>
+  <div>
+      <el-form ref="form" :model="form" label-width="120px">
+      <el-form-item label="MSISDN">
+        <el-input v-model="form.name"></el-input>
+      </el-form-item>
 
-      <v-spacer></v-spacer>
+      
+      <el-form-item label="Profile name">
+        <el-input v-model="form.name"></el-input>
+      </el-form-item>
+    
+      <el-form-item label="Features">
+        <el-checkbox-group v-model="form.type">
+          <el-checkbox label="CALL FORWARD UNCONDITIONAL (CFU)" name="type"></el-checkbox><br />
+          <el-checkbox label="CALL FORWARD UNREACHABLE (CFNRC)" name="type"></el-checkbox><br />
+          <el-checkbox label="CALL FORWARD ON BUSY (CFB)" name="type"></el-checkbox><br />
+          <el-checkbox label="CALL FORWARD ON NO REPLY (CFNRY)" name="type"></el-checkbox>
+        </el-checkbox-group>
+      </el-form-item>
 
-      <v-text-field
-        flat
-        outlined
-        clearable
-        hide-details
-        color="#8B222F"
-        label="Search"
-        prepend-inner-icon="mdi-magnify"
-      id="search_bar"></v-text-field>
-    </v-toolbar>
-
-    <v-card class="mt-10">
-      <h5>Put table here</h5>
-    </v-card>
-  </div>
+      <el-form-item label="Settings">
+        <el-checkbox-group v-model="form.type">
+          <el-checkbox label="ACTIVE (ACT)" name="type"></el-checkbox><br />
+          <el-checkbox label="SUSPENDED (SUS)" name="type"></el-checkbox><br />
+          <el-checkbox label="DETACHED SUBSCRIBERS (DTS)" name="type"></el-checkbox><br />
+        </el-checkbox-group>
+      </el-form-item>
+      
+      <el-form-item label="Notes">
+        <el-input type="textarea" v-model="form.desc"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">Save</el-button>
+        <el-button>Cancel</el-button>
+      </el-form-item>
+      </el-form>
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'Dashboard',
-  data() {
-    return {};
-  },
-};
+  export default {
+    data() {
+      return {
+        form: {
+          name: '',
+          region: '',
+          date1: '',
+          date2: '',
+          delivery: false,
+          type: [],
+          resource: '',
+          desc: ''
+        }
+      }
+    },
+    methods: {
+      onSubmit() {
+        console.log('submit!');
+      }
+    }
+  }
 </script>
-
-<style>
-#search_bar {
-  top: 10px;
-  left: 852px;
-  width: 319px;
-  height: 35px;
-  background: #FFFFFF 0% 0% no-repeat padding-box;
-  opacity: 1;
-}
-</style>
