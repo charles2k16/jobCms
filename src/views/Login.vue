cb5c2f01c9ed176d8f8a67af5606b79bf0336fe8
Charles Obeng <charlesobeng000@gmail.com>
2020-10-08 10:56:02 +0000

    login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,59 +1,84 @@
 <template>
-  <v-content>
-    <v-container fluid fill-height>
-      <v-layout align-center justify-center>
-        <v-flex xs12 sm8 md4>
-          <v-card class="elevation-12">
-            <v-toolbar color="primary" dark flat>
-              <v-toolbar-title>Login to your Account</v-toolbar-title>
-            </v-toolbar>
-            <v-card-text>
-              <v-form>
-                <v-text-field
-                  label="Email"
-                  name="email"
-                  prepend-icon="mdi-at"
-                  type="text"
-                ></v-text-field>
+  <div class="login">
+    <div class="login_form">
+      <div style="text-align:center; background:#8B222F;">
+        <router-link to="/">
+          <img
+            src="../assets/images/paic.png"
+            alt="logo"
+            style="width:180px;"
+          />
+        </router-link>
+      </div>
 
-                <v-text-field
-                  label="Password"
-                  name="password"
-                  prepend-icon="mdi-lock"
-                  type="password"
-                ></v-text-field>
-              </v-form>
-            </v-card-text>
-            <v-card-actions>
-              <v-btn 
-                :loading="loading" 
-                :disabled="loading"
-                block
-                color="primary"
-                class="white--text"
-                @click="login">
-                <v-icon left dark>mdi-login</v-icon>
-                Login
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-flex>
-      </v-layout>
-    </v-container>
-  </v-content>
+      <el-form :model="loginForm" ref="loginForm" status-icon :rules="rules">
+        <el-form-item prop="email" class="my-3">
+          <el-input
+            v-model="loginForm.email"
+            placeholder="yourname@email.com"
+            clearable
+            prefix-icon="el-icon-eleme"
+          />
+        </el-form-item>
+        <el-form-item prop="password" class="my-3">
+          <el-input
+            v-model="loginForm.password"
+            type="password"
+            placeholder="**********"
+            clearable
+            prefix-icon="el-icon-date"
+          />
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button
+          class="full-width"
+          type="primary"
+          :loading="btnLoading"
+          @click="signIn"
+          icon="el-icon-mouse"
+        >
+          Sign In</el-button
+        >
+      </span>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  data () {
+  data() {
     return {
-      loading: false
-    }
+      btnLoading: false,
+      loginForm: {
+        email: '',
+        password: '',
+      },
+      rules: {
+        email: [
+          {
+            required: true,
+            type: 'email',
+            message: 'Please enter valid email',
+            trigger: ['blur', 'change'],
+          },
+          { min: 5, message: 'Length should be 5 or more', trigger: 'blur' },
+        ],
+        password: [
+          { required: true, message: 'Please enter password', trigger: 'blur' },
+          {
+            min: 6,
+            message: 'Length should be 6 or more characters',
+            trigger: 'change',
+          },
+        ],
+      },
+    };
   },
   methods: {
-    login () {
-      this.loading = true
-    }
-  }
-}
+    signIn() {
+      this.btnLoading = true;
+    },
+  },
+};
 </script>
