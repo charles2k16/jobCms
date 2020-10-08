591c34cdccab0acc5e036f3f037f6b6eefac37f5
samuelbartels <samuel.bartels@paicbd.com>
2020-10-08 20:49:15 +0000

    added profile.js

--- src/api/profile.js
+++ src/api/profile.js
@@ -4,26 +4,41 @@ import config from "../config";
 export default {
   name: "ProfileService",
 
+  getFeatures() {
+    //TODO: Getting the features available for call-forwarding
+  },
+  getSettings() {
+    //status
+    //TODO: Getting the status catalogue
+  },
   getProfileList() {
-    let url = config.PROFILE_URL
-    return axios.get(url, {
-      auth: {
-        username: 'default',
-        password: '$PAiC2020@'
-      }
-    })
-      .then((response) => Promise.resolve(response.data))
-      .catch((error) => Promise.reject(error))
+    let url = config.PROFILE_URL;
+    return axios
+      .get(url, {
+        auth: {
+          username: "default",
+          password: "$PAiC2020@"
+        }
+      })
+      .then(response => Promise.resolve(response.data))
+      .catch(error => Promise.reject(error));
   },
   createProfile(profile) {
-    let url = config.PROFILE_URL
-    return axios.post(url, profile, {
-      auth: {
-        username: 'default',
-        password: '$PAiC2020@'
-      }
-    })
-      .then((response) => Promise.resolve(response.data))
-      .catch((error) => Promise.reject(error))
+    let url = config.PROFILE_URL;
+    return axios
+      .post(url, profile, {
+        auth: {
+          username: "default",
+          password: "$PAiC2020@"
+        }
+      })
+      .then(response => Promise.resolve(response.data))
+      .catch(error => Promise.reject(error));
+  },
+  updateProfile(profile) {
+    //TODO: Update profile by Id
+  },
+  applyProfile(profileId) {
+    //TODO: Apply profile settings to all subscribers associated with the profile
   }
-}
+};
